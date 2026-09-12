d2287479e379beef2a725d64c9f6474868f1d553
Dariusz Markiewicz <markiewicz.darek.dm@gmail.com>
2026-09-12 15:06:45 +0200

    Beautified

--- src/pages/sitemap-index.xml.ts
+++ src/pages/sitemap-index.xml.ts
@@ -1,2 +1,3 @@
-import type { APIRoute } from 'astro';
-export const GET: APIRoute = ({ site }) => new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>${new URL('/', site).href}</loc></url><url><loc>${new URL('/projecten/', site).href}</loc></url></urlset>`, { headers: { 'Content-Type': 'application/xml' } });
+import type {APIRoute} from 'astro';
+
+export const GET: APIRoute = ({site}) => new Response(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>${new URL('/', site).href}</loc></url><url><loc>${new URL('/projecten/', site).href}</loc></url></urlset>`, {headers: {'Content-Type': 'application/xml'}});
